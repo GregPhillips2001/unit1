b5d7fb5f8da5d2b47818fbd23f9907f1d3c9726e
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-09-05 14:33:03 -0400

    Updated from Brython Server: 9/5/2017 2:33:01 PM

--- Age.py
+++ Age.py
@@ -1,1 +1,10 @@
+#Greg Phillips
+#9/5/17
+#Age.py
 
+fullName = input("Write name here")
+age = int(input("Write age here"))
+firstName, lastName = fullName.split()
+print("Your first name has", len(firstName), "letters")
+print("Your last name has", len(lastName), "letters")
+print("Next year you will be", age+1, "years old")
