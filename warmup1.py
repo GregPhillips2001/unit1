42557a06b4e6dd070aeeb6fe2dce762bd471472e
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-09-06 13:39:41 -0400

    Updated from Brython Server: 9/6/2017 1:39:40 PM

--- warmup1.py
+++ warmup1.py
@@ -1,1 +1,8 @@
+#Greg Phillips
+#9/6/17
+#warmup1.py
 
+base = float(input("Write length of base here "))
+height = float(input("Write length of height here "))
+
+print("The area of the triangle is", base*height/2)
