f7090a3874dbe3af0c072b2305b36732931ad72b
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-09-08 14:55:06 -0400

    Updated from Brython Server: 9/8/2017 2:55:05 PM

--- eggs.py
+++ eggs.py
@@ -1,1 +1,9 @@
+#Greg Phillips
+#9/8/17
+#eggs.py - determines whethere a given number of eggs will fit into a given number of cartoons.
+
+eggs = input("number of eggs")
+eggCartons = input("number of egg cartons")
+
+holdsEggs = eggCartons*12
 
