e5ce3f7b90b598653978914b8b556a1e721f9645
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-09-11 14:24:20 -0400

    Updated from Brython Server: 9/11/2017 2:24:19 PM

--- quiz1.py
+++ quiz1.py
@@ -1,1 +1,12 @@
+#Greg Phillips
+#9/11/17
+#quiz1.py - unit 1 quiz
 
+name = input("Enter your name ")
+print(name)
+
+num1 = float(input("Enter a number: "))
+num2 = float(input("Enter another number: "))
+print("The sum is", num1+num2)
+print("The product is", num1*num2)
+print("Your lucky number is", num1*7+num2*7)
